2a4381600dabd872ad362e9027b4c66ba45abb4a
Aditya <aditya.gaonkar1907@gmail.com>
2025-03-30 12:42:06 +0530

    updated

--- index.php
+++ index.php
@@ -8,7 +8,6 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="public/styles/style.css">
     <link rel="stylesheet" href="public/styles/header.css">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/wow/1.1.2/wow.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
@@ -17,7 +16,7 @@
     </script>
     <style>
         .hero-section {
-            background: url('public/images/hero-bg2.jpg') no-repeat center center/cover;
+            background: url('public/images/hero-bg.jpg') no-repeat center center/cover;
             height: 190vh;
             display: flex;
             align-items: center;
